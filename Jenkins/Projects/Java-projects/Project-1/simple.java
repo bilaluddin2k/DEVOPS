669768c26abdc6d26078a369e9d3e324c146b379
bilaluddin2k <98716029+bilaluddin2k@users.noreply.github.com>
2024-06-30 21:50:34 +0530

    Update simple.java webhook trigger

--- Jenkins/Projects/Java-projects/Project-1/simple.java
+++ Jenkins/Projects/Java-projects/Project-1/simple.java
@@ -1,5 +1,5 @@
 public class simple {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Project new commit");
+        System.out.println("Welcome to Java Project new commit trigger webhook");
     }
 }
